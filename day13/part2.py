b60c77ab24b3955c3b983fec6a518c0824b1cb67
Enot1991 <90798546+Enot1991@users.noreply.github.com>
2022-05-07 17:44:21 +0700

    Add files via upload

---
+++ day13/part2.py
@@ -0,0 +1,59 @@
+f2 = open("output2.txt", "w")
+
+
+
+import re
+from itertools import permutations
+
+
+
+def find_max_happiness(v, e):
+    max_happiness = None
+    for perm in permutations(v):
+        happiness = 0
+        prev = None
+        for person in perm:
+            if prev is not None:
+                happiness += e[(prev, person)]
+                happiness += e[(person, prev)]
+            prev = person
+        happiness += e[(prev, perm[0])]
+        happiness += e[(perm[0], prev)]
+
+        if max_happiness is None or happiness > max_happiness:
+            max_happiness = happiness
+    return max_happiness
+
+
+
+input_pattern = r'(\w*) would (\w*) (\d*) .* (\w*)'
+v = set()
+e = dict()
+for line in open("input.txt"):
+    match = re.match(input_pattern, line.strip())
+    person1 = match[1]
+    person2 = match[4]
+    v.add(person1)
+    v.add(person2)
+    sign = 0
+    if match[2] == "lose":
+        sign = -1
+    elif match[2] == "gain":
+        sign = 1
+    else:
+        assert False
+    weight = int(match[3]) * sign
+    e[(person1, person2)] = weight
+
+
+
+you = "You"
+v.add(you)
+for person in v:
+    e[(you, person)] = 0
+    e[(person, you)] = 0
+    
+
+
+f2.write(str(find_max_happiness(v,e)))
+f2.close
